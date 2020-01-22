1a712d0b34955d085135c34fb2cdc92e729cf125
Samuel Adewole <samueladewole15@gmail.com>
2020-01-22 01:22:42 +0100

    init commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <input
+        v-model="title"
+        type="text"
+        name="title"
+        placeholder="Add Todo..."
+      />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddTodo',
+  data() {
+    return {
+      title: ''
+    };
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        id: Math.random(),
+        title: this.title,
+        completed: false
+      };
+
+      //send parameter
+      this.$emit('add-todo', newTodo);
+
+      // clear input
+      this.title = '';
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type='text'] {
+  flex: 10;
+  padding: 5px;
+}
+
+input[type='submit'] {
+  flex: 2;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="todo-item" v-bind:class="{ 'is-complete': todo.completed }">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{ todo.title }}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todo'],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #f00;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
